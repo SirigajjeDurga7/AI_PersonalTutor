6eaf37c9e7b19676879d25ea6f1654d88767b43e
SirigajjeDurga7 <sirigajjedurga@gmail.com>
2026-06-21 18:37:31 +0530

    Change email config to use TLS and disable SSL

--- backend-app/app.py
+++ backend-app/app.py
@@ -25,8 +25,8 @@ CORS(
 
 app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
 app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
